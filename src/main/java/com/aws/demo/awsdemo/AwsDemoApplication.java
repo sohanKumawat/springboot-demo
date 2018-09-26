be9bb26c208eb82088a00553f3e713d39001366d
sohank <sohan.kumawat@milkbasket.com>
2018-09-26 15:56:30 +0530

    added code

---
+++ src/main/java/com/aws/demo/awsdemo/AwsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.aws.demo.awsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDemoApplication.class, args);
+	}
+}
